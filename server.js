a2785f8b84d9a753bc32b151716a45329e1bad5c
Mayank Anil Dwivedi <mayankanildwivedi@gmail.com>
2016-10-07 01:39:38 +0530

    Updated with remote connection of mongo client
    Updated with remote connection of mongo client
    with username anrag and password Anrag

---
+++ server.js
@@ -0,0 +1,55 @@
+// server.js
+
+// BASE SETUP
+// =============================================================================
+
+// call the packages we need
+var express    = require('express');        // call express
+var app        = express();                 // define our app using express
+var bodyParser = require('body-parser');
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8082;
+
+var mongodb = require('mongodb');
+      // set our port
+      var Bear     = require('./app/models/bear');
+
+var uri = 'mongodb://anrag:Anrag@ds053146.mlab.com:53146/anrag';
+// ROUTES FOR OUR API
+var MongoClient = mongodb.MongoClient;
+// =============================================================================
+var router = express.Router();   
+MongoClient.connect(uri, function (err, db) {
+  if (err) {
+    console.log('Unable to connect to the mongoDB server. Error:', err);
+  } else {
+    //HURRAY!! We are connected. :)
+    console.log('Connection established to', uri);
+
+    // do some work here with the database.
+
+    //Close connection
+    db.close();
+  }
+});           // get an instance of the express Router
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });   
+});
+
+// more routes for our API will happen here
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
